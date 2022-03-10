eb73aac0355047412931f302dea6748af0d823a9
bernadette451 <t109590451@ntut.org.tw>
2022-03-10 23:10:15 +0800

    Practice05

---
+++ GitTutorial.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+bool Guess(int number){
+	return false;
+}
+
+int main()
+{
+
+}
---
+++ makefile
@@ -0,0 +1,2 @@
+GitTutorial:
+	g++ -o GitTutorial GitTutorial.cpp
